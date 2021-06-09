b6bd1411a03684b4827c8adb34653338e466a05f
DogeSSBM <dogessbm@gmail.com>
2021-06-09 18:52:46 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -1,28 +1,65 @@
 #include "Includes.h"
 
-typedef enum{P_PLAYER, P_BOT0, P_BOT1, P_BOT2}PlayerType;
+typedef enum{P_PLAYER, P_BOT0, P_BOT1, P_BOT2}PaddleType;
 
 typedef struct{
-	PlayerType type;
+	PaddleType type;
 	uint score;
 	Direction dir;
-	Coord pos;
-	Length len;
+	Coordf pos;
+	Lengthf len;
 	float speed;
-}Player;
+}Paddle;
 
-
-Player menu(const Length window, const uint scale)
+// draws a collection of strings evenly spaced between 2 points
+void spanTextList(const Coord start, const Coord stop, const uint num, const char **textList)
 {
-	uint selection = 0;
-	const uint numentries = 5;
-	const uint entryOffset = window.y/5;
-	const char *text[numentries] = {"Ponge", "Easy", "Medium", "Hard", "Multiplayer"};
+	if(num == 0)
+		return;
+	const Length step = coordDiv(coordSub(stop, start), num>1?num-1:1);
+	for(uint i = 0; i < num; i++){
+		drawTextCenteredCoord(coordOffset(start, coordMul(step, i)), textList[i]);
+	}
+}
+
+void spanTextListCentered(const Coord start, const Coord stop, const uint num, const char **textList)
+{
+	if(num == 0)
+		return;
+	const Length step = coordDiv(coordSub(stop, start), num+1);
+	for(uint i = 1; i < num+1; i++){
+		drawTextCenteredCoord(coordOffset(start, coordMul(step, i)), textList[i-1]);
+	}
+}
+
+Paddle menu(const Length window, const uint scale)
+{
+	// uint optSelected = 0;
+	// const static uint optNum = 5;
+	// const uint entryOffset = window.y/5;
+	Coord intvl[2] = {{window.x/4, 0}, (Coord){window.x/4, window.y}};
+	Offset off = {window.x/2, 0};
+	const char *optText[] = {"Easy", "Medium", "Hard", "Multiplayer"};
 	while(1){
 		Ticks frameStart = getTicks();
 		clear();
 
-		for
+		if(mouseBtnState(MOUSE_L)){
+			const uint i = coordDistSq(mouse.pos, intvl[0]) > coordDistSq(mouse.pos, intvl[1]);
+			setColor(WHITE);
+			fillCircleCoord(intvl[i], 3);
+			intvl[i] = coordOffset(intvl[i], mouse.vec);
+		}
+
+		if(mouseBtnState(MOUSE_R))
+			off = coordOffset(off, mouse.vec);
+
+		const uint num = 4;
+		setFontSize(24);
+		setFontColor(RED);
+		spanTextList(intvl[0], intvl[1], num, optText);
+		setFontColor(BLUE);
+		spanTextListCentered(coordOffset(intvl[0],off), coordOffset(intvl[1],off), num, optText);
 
 		draw();
 		events(frameStart + TPF);
